02dc2c8efd02a5c8f312e6812a2146061c3c1cfb
austinbhan <austinbhan@outlook.com>
2022-09-19 16:50:01 -0700

    titleCase

--- day-01/index.js
+++ day-01/index.js
@@ -4,4 +4,11 @@ function reverseSentence(str) {
     .map((word) => word.split('').reverse().join(''))
     .join(' ');
 }
-module.exports = { reverseSentence };
+
+function titleCase(str) {
+  return str.split(' ')
+    .map((element) => {
+      return element.charAt(0).toUpperCase() + element.slice(1).toLowerCase();
+    }).join(' ');
+}
+module.exports = { reverseSentence, titleCase };
