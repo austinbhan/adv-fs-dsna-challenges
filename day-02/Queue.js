3f4bc2584c320ef27f6ec3494046c8f1f290d13d
austinbhan <austinbhan@outlook.com>
2022-09-20 18:00:24 -0700

    enqueue in progress

--- day-02/Queue.js
+++ day-02/Queue.js
@@ -1,3 +1,11 @@
-class Queue {}
+class Queue {
+  list = [];
+
+  constructor(arr) {
+    if (arr) {
+      this.list = arr;
+    }
+  }
+}
 
 module.exports = { Queue };
---
+++ day-02/tests/Queue.test.js
@@ -0,0 +1,9 @@
+const { Queue } = require('../Queue.js');
+
+describe('Stack Test', () => {
+  it('Enqueue should add an item to the queue', () => {
+    const myArray = ['AliG', 'Borat', 'Bruno'];
+    expect(enqueue()).toEqual()
+  });
+});
+
